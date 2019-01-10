a7a001c76f19c2e29ceedaee6f810179588e7de8
joni <jonathan_erel@adsap.co>
2019-01-11 00:01:10 +0200

    add middleware to attach request data

--- Bp.Logging/EnrichWithRequestParams/EnrichWithRequestParamsMiddleware.cs
+++ Bp.Logging/EnrichWithRequestParams/EnrichWithRequestParamsMiddleware.cs
@@ -1,29 +1,28 @@
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
-using Microsoft.Extensions.Logging;
 using Serilog.Context;
 
 namespace Bp.Logging.EnrichWithRequestParams
 {
     public class EnrichWithRequestParamsMiddleware
     {
-        private readonly Logger<EnrichWithRequestParamsMiddleware> _logger;
         private readonly RequestDelegate _next;
 
-        public EnrichWithRequestParamsMiddleware(Logger<EnrichWithRequestParamsMiddleware> logger, RequestDelegate next)
+        public EnrichWithRequestParamsMiddleware(RequestDelegate next)
         {
-            _logger = logger;
             _next = next;
         }
 
-        public Task InvokeAsync(HttpContext context)
+        public async Task InvokeAsync(HttpContext context)
         {
-            LogContext.PushProperty("transactionId", context.TraceIdentifier);
+            using (LogContext.PushProperty("transactionId", context.TraceIdentifier))
+            {
+                await _next.Invoke(context);
+            }
 //            await context.Response.WriteAsync("------- Before ------ \n\r");
 //            await _next(context);
 
 //            await context.Response.WriteAsync("\n\r------- After ------");
-            return Task.CompletedTask;
         }
     }
 }
